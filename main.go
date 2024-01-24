f4ba5e58aa68fd259068fcafc2b9aa9f44e903a5
Muhammad Danish Khan <muhammad.danish1@careem.com>
2024-01-25 00:00:12 +0500

    first program

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+}
+
+// fmt is the library for input and output fmt. to use the member of packages
+// single line comment
+/* executable should have package main and fucntion main too
+Multi line comments
+*/
